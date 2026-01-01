687216edbed18e0a2570e7191b6426e67f8dbef7
Pranav Pandey <159560127+AkaPranav@users.noreply.github.com>
2026-01-01 09:08:56 +0000

    array in JavaScript

---
+++ 02_basics/01_array.js
@@ -0,0 +1,13 @@
+// ================== Arrays ================== 
+
+const mixArray = [0, 1, 2, 3, 4, 5, true, "Pranav"] 
+const arr = ["pranav", "sage"]
+                                    // -> These are some methods to create array 
+const arr1 = new Array(1, 2, 3, 4, 5)
+// in JS array can be of mixed datatypes 
+// in JS arrays are resizable 
+
+const myArray = [0, 1, 2, 3, 4, 5] 
+
+console.log(myArray[1]) //accessing elements in the array 
+
