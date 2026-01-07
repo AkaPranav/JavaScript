93b12772cf250b78bad4bfa9c5d4a59d02972834
Pranav Pandey <159560127+AkaPranav@users.noreply.github.com>
2026-01-07 09:35:59 +0000

    function with objects and array in JS

--- 03_basics/01_functions.js
+++ 03_basics/01_functions.js
@@ -37,3 +37,35 @@ function loginUserMessage(username = "sam"){
 console.log(loginUserMessage("Pranav Pandey"))
 console.log(loginUserMessage()) // undefined just logged in !
 
+// =============== function with objects & array ===============
+
+function calculateCartPrice(val1 , val2, ...num1){ // ... rest operator
+    return num1
+}
+
+console.log(calculateCartPrice(500, 200, 600, 350, 450)) // -> [ 600, 350, 450 ]
+
+const user = {
+    username : "Pranav",
+    price : 199
+}
+
+function handleObject(anyObject){
+    console.log(`Usernmae is ${anyObject.username} and Price is ${anyObject.price}`)
+}
+
+handleObject(user)
+handleObject({
+    username : "Raze",
+    price : "200"
+})
+
+
+const arr = [200, 300, 400, 500, 600, 700]
+
+function returnSecondValue(anyArray){
+    return anyArray[1]
+}
+
+console.log(returnSecondValue(arr))
+console.log(returnSecondValue([20, 30, 40, 50]))
