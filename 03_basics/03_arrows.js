84a0a4055faa15cabb402ac7db12eebaf51ae152
Pranav Pandey <159560127+AkaPranav@users.noreply.github.com>
2026-01-09 01:37:56 +0530

    Update 03_arrows.js

--- 03_basics/03_arrows.js
+++ 03_basics/03_arrows.js
@@ -9,16 +9,15 @@ const user = {
 
 }
 
-// user.welcomeMessage()
-// user.username = "sam"
-// user.welcomeMessage()
+user.welcomeMessage()
+user.username = "sam"
+user.welcomeMessage()
 
-// console.log(this);
+console.log(this);
 
-// function chai(){
-//     let username = "hitesh"
-//     console.log(this.username);
-// }
+function chai(){
+    let username = "hitesh"
+    console.log(this.username);}
 
 // chai()
 
