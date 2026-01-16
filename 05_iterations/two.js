7743f18b3ec1fabd574049731e28f211f1b90b60
Pranav Pandey <159560127+AkaPranav@users.noreply.github.com>
2026-01-16 18:55:22 +0000

    while & do while in JavaScript

---
+++ 05_iterations/two.js
@@ -0,0 +1,22 @@
+let index = 0
+
+while (index<=2) {
+    console.log(`Value of index ${index}`)
+    index += 2
+}
+
+let array = ["flash", "batman", "superman"]
+
+let arr = 0
+
+while (arr < array.length) {
+    console.log(`Value is ${array[arr]}`)
+    arr++
+}
+
+let score = 11
+
+do{
+    console.log(`Score is ${score}`)
+    score++
+}while(score <= 3)
